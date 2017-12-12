682922ac8625b4659be4777dc1094799b7566da2
Truong <kevin.truong@yale.edu>
2017-12-11 23:09:49 -0500

    modified tok

--- tok.py
+++ tok.py
@@ -3,7 +3,6 @@ import string
 import nltk.tokenize 
 import os 
 import sys
-# Usage ./tokenize directory
 
 # no punctuation
 
@@ -18,17 +17,32 @@ def speech_parse(file):
                 line = re.sub("-", " ", line)
                 tokenized = nltk.word_tokenize(line)
                 text.append(tokenized)
+        text = [x for x in text if x != []]
+	return text 
             
 def clean_speech(lines):
-   
-    print lines 
+    tokens = ["<s>"]
+    punct = ["?", ".", "!",".."," ", '"']
+    line = nltk.word_tokenize(lines)
+    print line
+    for i in range(len(line)):
+	if line[i] in punct:
+	    tokens.extend(["</s>", "<s>"])
+	elif line[i] != ",":
+	    tokens.extend([line[i].lower()])
+    tokens.extend(["</s>"])
+    if len(tokens) > 2:
+	while (tokens[len(tokens) - 2] == "<s>"):
+	    tokens = tokens[:len(tokens) - 2]
+	while (tokens[1] == "</s>"):
+		    tokens = tokens[2:]
+    return tokens
 
 
-def dir_parse():
-    for filename in os.listdir(sys.argv[1]):
-	if filename.endswith(".txt"): 
-	    speech_parse(filename)
-	else:
-	    continue
-
+for filename in os.listdir(sys.argv[1]):
+    if filename.endswith(".txt"): 
+	for i in speech_parse(filename):
+		print clean_speech(i) 
+    else:
+   	print "Usage: python tok.py directory"	
 
